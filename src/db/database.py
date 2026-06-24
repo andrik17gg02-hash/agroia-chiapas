b2e810f8b0b2fb1e6f5d7b4d8fde97babd7da29a
Andrik <andrik17gg02@gmail.com>
2026-06-24 14:46:24 -0600

    fix: configurar SSL correctamente para pg8000 con Neon

--- src/db/database.py
+++ src/db/database.py
@@ -1,9 +1,15 @@
+import ssl
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+ssl_context = ssl.create_default_context()
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"ssl_context": ssl_context},
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
